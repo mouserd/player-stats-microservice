57be4431fa759a5f84f927f49e9ec262da80cf22
David Mouser <David.Mouser@afl.com.au>
2015-03-06 17:42:52 +1100

    Minor updates.

--- src/test/java/com/pixelus/playerstats/PlayerStatsApplicationTests.java
+++ src/test/java/com/pixelus/playerstats/PlayerStatsApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = PlayerStatsApplication.class)
 @WebAppConfiguration
-public class PlayerApplicationTests {
+public class PlayerStatsApplicationTests {
 
 	@Test
 	public void contextLoads() {
