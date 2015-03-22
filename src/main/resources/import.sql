716a333c9667929fd99f5b90fd825d0e7f40e33b
David Mouser <David.Mouser@afl.com.au>
2015-03-22 19:43:02 +1100

    Added some more sample data.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,5 @@
-insert into player_stats(id, career_games, career_goals) values ('CD_P3001292', 283, 398);
-insert into player_stats(id, career_games, career_goals) values ('CD_P3001293', 120, 18);
+insert into player_stats(id, career_games, career_goals) values ('CD_I220001', 358, 268);
+insert into player_stats(id, career_games, career_goals) values ('CD_I240290', 225, 191);
+insert into player_stats(id, career_games, career_goals) values ('CD_I250267', 117, 170);
+insert into player_stats(id, career_games, career_goals) values ('CD_I261396', 161, 115);
+insert into player_stats(id, career_games, career_goals) values ('CD_I271078', 40, 104);
